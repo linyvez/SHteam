90a2444fd1b34fb5f490051391d6070aeaf182d1
Andrii <bilyjandrij4@gmail.com>
2026-05-06 01:08:06 +0300

    Change catalog path to /catalog

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -10,7 +10,7 @@ function App() {
       <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
 
-      <Route path="/" element={<Catalog />} />
+      <Route path="/catalog" element={<Catalog />} />
 
       <Route path="/profile" element={<Profile />} />
     </Routes>
